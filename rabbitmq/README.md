566cea4a8720e153249a7df0caea5dd1a7dfe482
He Chen <hchen584@bloomberg.net>
2020-10-04 16:57:40 -0400

    rabbitmq in Nodejs

---
+++ rabbitmq/README.md
@@ -0,0 +1,7 @@
+# rabbitmq
+
+To start a rabbitmq server: `docker run --name rabbitmq -p 5672:5672 rabbitmq`
+
+To start the publisher: `npm run publish`
+
+To start the consumer: `npm run consume`
---
+++ rabbitmq/consumer.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib")
+
+connect();
+
+async function connect() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672")
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message => {
+            const input = JSON.parse(message.content.toString());
+            console.log(`Received job with inut ${input.number}`);
+
+            // if (input.number == 7)
+            //     channel.ack(message);
+            channel.ack(message);
+        })
+
+        console.log("Waiting for messages...")
+    }
+    catch (ex) {
+        console.error(ex)
+    }
+}
